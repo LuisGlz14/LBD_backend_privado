5913754659baf254fd500feb285b4b15d316ffad
omarrb76 <omarshark23@gmail.com>
2020-11-22 13:35:14 -0600

    En este punto ya funciona el CRUD para todas nuestras tablas, ahora solo faltan las consultas chidas que nos piden datos. Eso lo hare hasta que tengan el frontend

--- routes/rutas.js
+++ routes/rutas.js
@@ -158,4 +158,341 @@ router.delete('/delete_colonia', [
     }));
 });
 
+// Operaciones para domicilio
+router.put('/put_domicilio', [
+
+    body('cp').not().isEmpty().isNumeric(),
+    body('numero').not().isEmpty().isNumeric(),
+    body('calle').not().isEmpty().isString(),
+    body('curp').not().isEmpty().isString()
+
+], (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.json({ success: false, err: JSON.stringify(errors) })
+        return
+    }
+
+    const cuerpo = req.body;
+    user.createDomicilio(connection, cuerpo, (data => {
+        res.json(data);
+    }));
+});
+
+router.get('/get_domicilio', [], (req, res) => {
+    user.getDomicilio(connection, (data => {
+        res.json(data);
+    }));
+});
+
+router.post('/update_domicilio', [
+
+    body('id_domicilio').not().isEmpty().isNumeric(),
+    body('cp').not().isEmpty().isNumeric(),
+    body('numero').not().isEmpty().isNumeric(),
+    body('calle').not().isEmpty().isString(),
+    body('curp').not().isEmpty().isString()
+
+], (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.json({ success: false, err: JSON.stringify(errors) })
+        return
+    }
+
+    const cuerpo = req.body;
+    user.updateDomicilio(connection, cuerpo, (data => {
+        res.json(data);
+    }));
+});
+
+router.delete('/delete_domicilio', [
+
+    body('id_domicilio').not().isEmpty().isNumeric()
+
+], (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.json({ success: false, err: JSON.stringify(errors) })
+        return
+    }
+
+    const cuerpo = req.body;
+    user.deleteDomicilio(connection, cuerpo, (data => {
+        res.json(data);
+    }));
+});
+
+// Operaciones para planta eléctrica
+router.put('/put_planta', [
+
+    body('tipo_renovable').not().isEmpty().isString(),
+    body('recurso_alim').not().isEmpty().isString(),
+    body('tipo_planta').not().isEmpty().isString(),
+    body('cant_energ_gen').not().isEmpty().isNumeric(),
+    body('emisiones_generadas').not().isEmpty().isNumeric(),
+    body('id_domicilio').not().isEmpty().isNumeric()
+
+], (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.json({ success: false, err: JSON.stringify(errors) })
+        return
+    }
+
+    const cuerpo = req.body;
+    user.createPlanta(connection, cuerpo, (data => {
+        res.json(data);
+    }));
+});
+
+router.get('/get_planta', [], (req, res) => {
+    user.getPlanta(connection, (data => {
+        res.json(data);
+    }));
+});
+
+router.post('/update_planta', [
+
+    body('id_planta').not().isEmpty().isNumeric(),
+    body('tipo_renovable').not().isEmpty().isString(),
+    body('recurso_alim').not().isEmpty().isString(),
+    body('tipo_planta').not().isEmpty().isString(),
+    body('cant_energ_gen').not().isEmpty().isNumeric(),
+    body('emisiones_generadas').not().isEmpty().isNumeric(),
+    body('id_domicilio').not().isEmpty().isNumeric()
+
+], (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.json({ success: false, err: JSON.stringify(errors) })
+        return
+    }
+
+    const cuerpo = req.body;
+    user.updatePlanta(connection, cuerpo, (data => {
+        res.json(data);
+    }));
+});
+
+router.delete('/delete_planta', [
+
+    body('id_planta').not().isEmpty().isNumeric()
+
+], (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.json({ success: false, err: JSON.stringify(errors) })
+        return
+    }
+
+    const cuerpo = req.body;
+    user.deletePlanta(connection, cuerpo, (data => {
+        res.json(data);
+    }));
+});
+
+// Operaciones para recibo
+router.put('/put_recibo', [
+
+    body('id_domicilio').not().isEmpty().isNumeric(),
+    body('fecha').not().isEmpty().isString(),
+    body('consumo').not().isEmpty().isNumeric(),
+    body('cant_pagar').not().isEmpty().isNumeric()
+
+], (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.json({ success: false, err: JSON.stringify(errors) })
+        return
+    }
+
+    const cuerpo = req.body;
+    user.createRecibo(connection, cuerpo, (data => {
+        res.json(data);
+    }));
+});
+
+router.get('/get_recibo', [], (req, res) => {
+    user.getRecibo(connection, (data => {
+        res.json(data);
+    }));
+});
+
+router.delete('/delete_recibo', [
+
+    body('id_recibo').not().isEmpty().isNumeric()
+
+], (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.json({ success: false, err: JSON.stringify(errors) })
+        return
+    }
+
+    const cuerpo = req.body;
+    user.deleteRecibo(connection, cuerpo, (data => {
+        res.json(data);
+    }));
+});
+
+// Operaciones para disp_energ_ren
+router.put('/put_disp', [
+
+    body('numero_disp').not().isEmpty().isNumeric(),
+    body('id_domicilio').not().isEmpty().isNumeric(),
+    body('energia_generada').not().isEmpty().isNumeric(),
+    body('tipo_renovable').not().isEmpty().isString()
+
+], (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.json({ success: false, err: JSON.stringify(errors) })
+        return
+    }
+
+    const cuerpo = req.body;
+    user.createDispositivo(connection, cuerpo, (data => {
+        res.json(data);
+    }));
+});
+
+router.get('/get_disp', [], (req, res) => {
+    user.getDispositivo(connection, (data => {
+        res.json(data);
+    }));
+});
+
+router.delete('/delete_disp', [
+
+    body('numero_disp').not().isEmpty().isNumeric(),
+    body('id_domicilio').not().isEmpty().isNumeric()
+
+], (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.json({ success: false, err: JSON.stringify(errors) })
+        return
+    }
+
+    const cuerpo = req.body;
+    user.deleteDispositivo(connection, cuerpo, (data => {
+        res.json(data);
+    }));
+});
+
+// Operaciones para viven
+router.put('/put_viven', [
+
+    body('curp').not().isEmpty().isString(),
+    body('id_domicilio').not().isEmpty().isNumeric()
+
+], (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.json({ success: false, err: JSON.stringify(errors) })
+        return
+    }
+
+    const cuerpo = req.body;
+    user.createViven(connection, cuerpo, (data => {
+        res.json(data);
+    }));
+});
+
+router.get('/get_viven', [], (req, res) => {
+    user.getViven(connection, (data => {
+        res.json(data);
+    }));
+});
+
+router.delete('/delete_viven', [
+
+    body('curp').not().isEmpty().isString(),
+    body('id_domicilio').not().isEmpty().isNumeric()
+
+], (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.json({ success: false, err: JSON.stringify(errors) })
+        return
+    }
+
+    const cuerpo = req.body;
+    user.deleteViven(connection, cuerpo, (data => {
+        res.json(data);
+    }));
+});
+
+// Operaciones para alimenta
+router.put('/put_alimenta', [
+
+    body('id_planta').not().isEmpty().isNumeric(),
+    body('cp').not().isEmpty().isNumeric()
+
+], (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.json({ success: false, err: JSON.stringify(errors) })
+        return
+    }
+
+    const cuerpo = req.body;
+    user.createAlimenta(connection, cuerpo, (data => {
+        res.json(data);
+    }));
+});
+
+router.get('/get_alimenta', [], (req, res) => {
+    user.getAlimenta(connection, (data => {
+        res.json(data);
+    }));
+});
+
+router.delete('/delete_alimenta', [
+
+    body('curp').not().isEmpty().isString(),
+    body('id_domicilio').not().isEmpty().isNumeric()
+
+], (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.json({ success: false, err: JSON.stringify(errors) })
+        return
+    }
+
+    const cuerpo = req.body;
+    user.deleteAlimenta(connection, cuerpo, (data => {
+        res.json(data);
+    }));
+});
+
 module.exports = router;
--- user.model.js
+++ user.model.js
@@ -109,4 +109,270 @@ module.exports = {
         });
     },
 
+    createDomicilio: (connection, body, callback) => {
+        const query = 
+        `INSERT INTO DOMICILIO (cp, numero, calle, curp) values (${body.cp}, ${body.numero}, "${body.calle}", "${body.curp}");`;
+        connection.query(query, body, (err, results) => {
+            if (err) {
+                callback({ array: null, id: null, success: false, err: JSON.stringify(err) });
+                return;
+            }
+
+            callback({ array: null, id: null, success: true });
+        });
+    },
+
+    getDomicilio: (connection, callback) => {
+        const query = `SELECT * FROM Domicilio`;
+        connection.query(query, (err, results) => {
+            if (err) {
+                callback({ array: null, id: null, success: false, err: JSON.stringify(err) });
+                return;
+            }
+
+            callback({ array: results, id: null, success: true });
+        });
+    },
+
+    updateDomicilio: (connection, body, callback) => {
+        const query = 
+        `UPDATE DOMICILIO SET 
+        cp = ${body.cp}, 
+        numero = ${body.numero}, 
+        calle = "${body.calle}", 
+        curp = "${body.curp}"
+        WHERE id_domicilio = ${body.id_domicilio}`;
+        connection.query(query, (err, results) => {
+            if (err) {
+                callback({ array: null, id: null, success: false, err: JSON.stringify(err) });
+                return;
+            }
+
+            callback({ array: results, id: null, success: true });
+        });
+    },
+
+    deleteDomicilio: (connection, body, callback) => {
+        const query = `DELETE FROM Domicilio WHERE id_domicilio = ${body.id_domicilio}`;
+        connection.query(query, (err, results) => {
+            if (err) {
+                callback({ array: null, id: null, success: false, err: JSON.stringify(err) });
+                return;
+            }
+
+            callback({ array: results, id: null, success: true });
+        });
+    },
+
+    createPlanta: (connection, body, callback) => {
+        const query = 
+        `INSERT INTO PLANTA_ELECTRICA 
+        (tipo_renovable, recurso_alim, tipo_planta, cant_energ_gen, emisiones_generadas, id_domicilio)
+        values ("${body.tipo_renovable}", "${body.recurso_alim}", "${body.tipo_planta}", ${body.cant_energ_gen}, ${body.emisiones_generadas}, ${body.id_domicilio});`;
+        connection.query(query, body, (err, results) => {
+            if (err) {
+                callback({ array: null, id: null, success: false, err: JSON.stringify(err) });
+                return;
+            }
+
+            callback({ array: null, id: null, success: true });
+        });
+    },
+
+    getPlanta: (connection, callback) => {
+        const query = `SELECT * FROM PLANTA_ELECTRICA`;
+        connection.query(query, (err, results) => {
+            if (err) {
+                callback({ array: null, id: null, success: false, err: JSON.stringify(err) });
+                return;
+            }
+
+            callback({ array: results, id: null, success: true });
+        });
+    },
+
+    updatePlanta: (connection, body, callback) => {
+        const query = 
+        `UPDATE PLANTA_ELECTRICA SET 
+        tipo_renovable = "${body.tipo_renovable}", 
+        recurso_alim = "${body.recurso_alim}", 
+        tipo_planta = "${body.tipo_planta}", 
+        cant_energ_gen = ${body.cant_energ_gen},
+        emisiones_generadas = ${body.emisiones_generadas}
+        WHERE id_planta = ${body.id_planta}`;
+        connection.query(query, (err, results) => {
+            if (err) {
+                callback({ array: null, id: null, success: false, err: JSON.stringify(err) });
+                return;
+            }
+
+            callback({ array: results, id: null, success: true });
+        });
+    },
+
+    deletePlanta: (connection, body, callback) => {
+        const query = `DELETE FROM PLANTA_ELECTRICA WHERE id_planta = ${body.id_planta}`;
+        connection.query(query, (err, results) => {
+            if (err) {
+                callback({ array: null, id: null, success: false, err: JSON.stringify(err) });
+                return;
+            }
+
+            callback({ array: results, id: null, success: true });
+        });
+    },
+
+    createRecibo: (connection, body, callback) => {
+        const query = 
+        `INSERT INTO RECIBO (id_domicilio, fecha, consumo_energia, cant_pagar)
+        values (${body.id_domicilio}, "${body.fecha}", ${body.consumo}, ${body.cant_pagar});`;
+        connection.query(query, body, (err, results) => {
+            if (err) {
+                callback({ array: null, id: null, success: false, err: JSON.stringify(err) });
+                return;
+            }
+
+            callback({ array: null, id: null, success: true });
+        });
+    },
+
+    getRecibo: (connection, callback) => {
+        const query = `SELECT * FROM RECIBO`;
+        connection.query(query, (err, results) => {
+            if (err) {
+                callback({ array: null, id: null, success: false, err: JSON.stringify(err) });
+                return;
+            }
+
+            callback({ array: results, id: null, success: true });
+        });
+    },
+
+    deleteRecibo: (connection, body, callback) => {
+        const query = `DELETE FROM RECIBO WHERE id_recibo = ${body.id_recibo}`;
+        connection.query(query, (err, results) => {
+            if (err) {
+                callback({ array: null, id: null, success: false, err: JSON.stringify(err) });
+                return;
+            }
+
+            callback({ array: results, id: null, success: true });
+        });
+    },
+
+    createDispositivo: (connection, body, callback) => {
+        const query = 
+        `INSERT INTO DISPOSITIVO_ENERGIA_RENOVABLE SET ?`;
+        connection.query(query, body, (err, results) => {
+            if (err) {
+                callback({ array: null, id: null, success: false, err: JSON.stringify(err) });
+                return;
+            }
+
+            callback({ array: null, id: null, success: true });
+        });
+    },
+
+    getDispositivo: (connection, callback) => {
+        const query = `SELECT * FROM DISPOSITIVO_ENERGIA_RENOVABLE`;
+        connection.query(query, (err, results) => {
+            if (err) {
+                callback({ array: null, id: null, success: false, err: JSON.stringify(err) });
+                return;
+            }
+
+            callback({ array: results, id: null, success: true });
+        });
+    },
+
+    deleteDispositivo: (connection, body, callback) => {
+        const query = 
+        `DELETE FROM DISPOSITIVO_ENERGIA_RENOVABLE WHERE numero_disp = ${body.numero_disp} AND
+        id_domicilio = ${body.id_domicilio}`;
+        connection.query(query, (err, results) => {
+            if (err) {
+                callback({ array: null, id: null, success: false, err: JSON.stringify(err) });
+                return;
+            }
+
+            callback({ array: results, id: null, success: true });
+        });
+    },
+
+    createViven: (connection, body, callback) => {
+        const query = 
+        `INSERT INTO VIVEN SET ?`;
+        connection.query(query, body, (err, results) => {
+            if (err) {
+                callback({ array: null, id: null, success: false, err: JSON.stringify(err) });
+                return;
+            }
+
+            callback({ array: null, id: null, success: true });
+        });
+    },
+
+    getViven: (connection, callback) => {
+        const query = `SELECT * FROM VIVEN`;
+        connection.query(query, (err, results) => {
+            if (err) {
+                callback({ array: null, id: null, success: false, err: JSON.stringify(err) });
+                return;
+            }
+
+            callback({ array: results, id: null, success: true });
+        });
+    },
+
+    deleteViven: (connection, body, callback) => {
+        const query = 
+        `DELETE FROM VIVEN WHERE curp = "${body.curp}" AND id_domicilio = ${body.id_domicilio}`;
+        connection.query(query, (err, results) => {
+            if (err) {
+                callback({ array: null, id: null, success: false, err: JSON.stringify(err) });
+                return;
+            }
+
+            callback({ array: results, id: null, success: true });
+        });
+    },
+
+    createAlimenta: (connection, body, callback) => {
+        const query = 
+        `INSERT INTO ALIMENTA SET ?`;
+        connection.query(query, body, (err, results) => {
+            if (err) {
+                callback({ array: null, id: null, success: false, err: JSON.stringify(err) });
+                return;
+            }
+
+            callback({ array: null, id: null, success: true });
+        });
+    },
+
+    getAlimenta: (connection, callback) => {
+        const query = `SELECT * FROM ALIMENTA`;
+        connection.query(query, (err, results) => {
+            if (err) {
+                callback({ array: null, id: null, success: false, err: JSON.stringify(err) });
+                return;
+            }
+
+            callback({ array: results, id: null, success: true });
+        });
+    },
+
+    deleteAlimenta: (connection, body, callback) => {
+        const query = 
+        `DELETE FROM ALIMENTA WHERE id_planta = ${body.id_planta} AND cp = ${body.cp}`;
+        connection.query(query, (err, results) => {
+            if (err) {
+                callback({ array: null, id: null, success: false, err: JSON.stringify(err) });
+                return;
+            }
+
+            callback({ array: results, id: null, success: true });
+        });
+    },
+
 }
